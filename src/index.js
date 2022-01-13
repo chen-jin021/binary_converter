e6d9cbee982a147c07a0da979d382373a0289420
Chen Jin <53110673+chen-jin021@users.noreply.github.com>
2022-01-13 14:23:24 -0500

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,48 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const Converter = (props) => {
+  const [bits, setBits] = useState(new Array(8).fill(0));
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  const s = bits.join(""); //joins all the bits to a string
+  const dec = parseInt(s, 2);
+
+  //function to handle toggling
+  function toggleBit(i, evt) {
+    const copy = bits.slice();
+    // if (copy[i]) {
+    //   copy[i] = 0;
+    // } else {
+    //   copy[i] = 1;
+    // }
+    copy[i] = copy[i] === 0 ? 1 : 0;
+    setBits(copy);
+  }
+  //an array of Bit component
+  const bitComponents = bits.map((val, i) => {
+    //our event handler expects only one parameter
+    //solution: use arrow so we reduce the argument to just 'evt'
+    return <Bit toggleBit={(evt) => toggleBit(i, evt)} val={val} key={i} />;
+  });
+  return (
+    <>
+      <h1>bin to dec</h1>
+      {bitComponents}
+      <Dec dec={dec} />
+    </>
+  );
+};
+
+const Dec = (props) => {
+  return <div>{props.dec}</div>;
+};
+
+const Bit = (props) => {
+  return (
+    <div onClick={props.toggleBit} className="bit">
+      {props.val}
+    </div>
+  );
+};
+ReactDOM.render(<Converter />, document.getElementById("root"));
